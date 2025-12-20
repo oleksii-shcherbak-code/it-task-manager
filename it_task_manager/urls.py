b6e03d8265d4b3f30e7d488d00de2f22ea1ed6ef
Oleksii Shcherbak <qksardas001@mail.com>
2025-12-20 19:39:42 +0200

    Add urls for registration and profile

--- it_task_manager/urls.py
+++ it_task_manager/urls.py
@@ -17,9 +17,19 @@ Including another URLconf
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+from tasks.views import RegisterView, ProfileView, ProfileUpdateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include(debug_toolbar.urls)),
     path("", include("tasks.urls")),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path("profile/edit/", ProfileUpdateView.as_view(), name="profile-edit"),
+    path("password_change/", auth_views.PasswordChangeView.as_view(template_name="registration/password_change.html"), name="password_change"),
+    path("password_reset/", auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"), name="password_reset"),
 ]
