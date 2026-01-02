60e38ca03edcdb230072d76556154115be2b26c7
Oleksii Shcherbak <qksardas001@mail.com>
2026-01-02 11:38:33 +0200

    Refactor forms: unify name field validation, simplify password handling, optimize username help_texts

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,41 +1,35 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
 from .validators import validate_username, validate_only_letters
 
 Worker = get_user_model()
 
 
-class WorkerCreationForm(UserCreationForm):
-    first_name = forms.CharField(validators=[validate_only_letters], required=True)
-    last_name = forms.CharField(validators=[validate_only_letters], required=True)
+class OnlyLettersCharField(forms.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("validators", [validate_only_letters])
+        super().__init__(*args, **kwargs)
+
+
+class BaseWorkerForm(forms.ModelForm):
+    first_name = OnlyLettersCharField(required=True)
+    last_name = OnlyLettersCharField(required=True)
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = Worker
         fields = ("username", "first_name", "last_name", "email", "position")
-        help_texts = {"username": ""}
+        help_texts = {"username": None}
 
     def clean_username(self):
         validate_username(self.cleaned_data.get("username"))
         return self.cleaned_data.get("username")
 
-    def clean_password1(self):
-        password = self.cleaned_data.get("password1")
-        validate_password(password, self.instance)
-        return password
+
+class WorkerCreationForm(UserCreationForm, BaseWorkerForm):
+    pass
 
 
-class WorkerChangeForm(UserChangeForm):
+class WorkerChangeForm(UserChangeForm, BaseWorkerForm):
     password = None
-    first_name = forms.CharField(validators=[validate_only_letters], required=True)
-    last_name = forms.CharField(validators=[validate_only_letters], required=True)
-
-    class Meta:
-        model = Worker
-        fields = ("username", "first_name", "last_name", "email", "position")
-        help_texts = {"username": ""}
-
-    def clean_username(self):
-        validate_username(self.cleaned_data.get("username"))
-        return self.cleaned_data.get("username")
